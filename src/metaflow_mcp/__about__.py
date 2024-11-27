6ad6001c75dfb1b277d1984716ca0eb8d5ca0cbe
Bryan Galvin <bcgalvin@gmail.com>
2024-11-27 09:08:34 -0800

    configuring hatch

--- src/metaflow_mcp/__about__.py
+++
@@ -1,4 +0,0 @@
-# SPDX-FileCopyrightText: 2024-present Bryan Galvin <bcgalvin@gmail.com>
-#
-# SPDX-License-Identifier: MIT
-__version__ = "0.0.1"
--- src/metaflow_mcp/__init__.py
+++ src/metaflow_mcp/__init__.py
@@ -1,3 +1,1 @@
-# SPDX-FileCopyrightText: 2024-present Bryan Galvin <bcgalvin@gmail.com>
-#
-# SPDX-License-Identifier: MIT
+__version__ = "0.0.1"
